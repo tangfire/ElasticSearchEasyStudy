5a27c2973a321c2c63999a5b3d2d92d043e63c74
tangfire <52628216@qq.com>
2025-05-06 21:55:33 +0800

    Initial commit

---
+++ src/main/java/com/fire/elasticsearcheasystudy/ElasticSearchEasyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.fire.elasticsearcheasystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchEasyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchEasyStudyApplication.class, args);
+	}
+
+}
